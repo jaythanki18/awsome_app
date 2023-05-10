a6c0ecc2770513cf20b705e06bb9907bdb8e1533
jaythanki18 <21CE143@charusat.edu.in>
2023-05-10 15:53:04 +0530

    ch4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,62 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _nameController=TextEditingController();
+  var myText="Login";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Awsome app"),
       ),
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/login.jpg",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter username",
+                      labelText: "Username"
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -25,10 +71,9 @@ class HomePage extends StatelessWidget {
             UserAccountsDrawerHeader(
                 accountName: Text("Jay Thanki"),
                 accountEmail: Text("jaythanki200@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage("assets/jay.jpg"),
-              )
-            ),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage("assets/jay.jpg"),
+                )),
             ListTile(
               leading: Icon(Icons.person),
               title: Text("Account"),
@@ -51,8 +96,11 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.edit),
+        onPressed: () {
+          myText=_nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.refresh),
       ),
     );
   }
