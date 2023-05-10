2b176189b0f63a20eb81109f4de447d7465efaf7
jaythanki18 <21CE143@charusat.edu.in>
2023-05-10 10:57:33 +0530

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: "Awsome app",
-    home: HomePage()
-  ));
+void main() {
+  runApp(MaterialApp(title: "Awsome app", home: HomePage()));
 }
 
 class HomePage extends StatelessWidget {
@@ -16,8 +13,29 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awsome app"),
       ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, blurRadius: 5, offset: Offset(2.0, 5.0))
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.red])),
+          child: Text(
+            "I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20),
+          ),
+        ),
       ),
     );
   }
